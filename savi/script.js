463229a872922568ecc04e886f322cfbcb0f74de
yancita45678 <yanci1553@gmail.com>
2026-02-25 22:31:50 -0600

    Add files via upload

---
+++ savi/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("whatsappBtn");
+
+    btn.addEventListener("click", function() {
+        alert("Serás redirigido a WhatsApp para enviar tu CV a Savï 🌿");
+    });
+});
